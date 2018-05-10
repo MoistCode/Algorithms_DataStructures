8a3614fdf3004da353994131b6aea048ed7f78e2
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-10 11:58:35 -0700

    added binary search for ruby

---
+++ Searching/Ruby/binary_search.rb
@@ -0,0 +1,12 @@
+# Given a sorted array 
+# Find element 'x' 
+
+# Binary Search 
+# Divide search interval in half
+# Begin with the midpoint 
+# If the value is less than 'x' 
+    # Search right 
+# If the value is greater than 'x' 
+    # Search left 
+# If the value is equal to 'x' 
+    # Return
