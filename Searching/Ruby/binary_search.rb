92729fe0a973aaa8213f43a05b3e73334b997470
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-10 12:37:57 -0700

    dem binaries

--- Searching/Ruby/binary_search.rb
+++ Searching/Ruby/binary_search.rb
@@ -6,7 +6,16 @@
 # Begin with the midpoint 
 # If the value is less than 'x' 
     # Search right 
+    # No need to keep track of index 
 # If the value is greater than 'x' 
     # Search left 
+    # Index is equal index of midpoint +  
 # If the value is equal to 'x' 
-    # Return
+    # Return index 
+
+def binary_search(arr)
+    
+    midpoint_index = arr.length / 2 
+
+    
+end
