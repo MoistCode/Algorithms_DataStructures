15ef5dc98cccfdc29078dbf1e28edd6e362535ae
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-11 20:05:14 -0700

    ruby jump search

--- Searching/Ruby/jump_search.rb
+++ Searching/Ruby/jump_search.rb
@@ -1,4 +1,26 @@
 def jump_search(arr, val, arrLength)
     step = Math.sqrt(arrLength)
     prev = 0
+
+    while arr[(step, arrLength).min] < val
+        prev = step 
+        step += Math.sqrt(arrLength)
+        if prev >= arrLength 
+            return -1
+        end
+    end
+
+    while arr[prev] < val 
+        prev += 1 
+
+        if prev == (step, arrLength).min 
+            return -1
+        end
+    end
+
+    if arr[prev] == val 
+        return prev
+    end
+
+    return -1
 end
