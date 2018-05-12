b31bfe973acc917834babfd83eeeb4bd4b8e4515
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-11 20:10:49 -0700

    coersion

--- Searching/Ruby/jump_search.rb
+++ Searching/Ruby/jump_search.rb
@@ -1,10 +1,10 @@
 def jump_search(arr, val, arrLength)
-    step = Math.sqrt(arrLength)
+    step = Math.sqrt(arrLength).to_i
     prev = 0
 
-    while arr[(step, arrLength).min] < val
+    while arr[[step, arrLength].min] < val
         prev = step 
-        step += Math.sqrt(arrLength)
+        step += Math.sqrt(arrLength).to_i
         if prev >= arrLength 
             return -1
         end
@@ -13,7 +13,7 @@ def jump_search(arr, val, arrLength)
     while arr[prev] < val 
         prev += 1 
 
-        if prev == (step, arrLength).min 
+        if prev == [step, arrLength].min 
             return -1
         end
     end
@@ -24,3 +24,8 @@ def jump_search(arr, val, arrLength)
 
     return -1
 end
+
+arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 
+    34, 55, 89, 144, 233, 377, 610 ]
+
+p jump_search(arr, 55, arr.length)
