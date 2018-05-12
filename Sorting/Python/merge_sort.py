e3845161fb922b505ec93c345430f3b060515a69
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-12 16:41:09 -0700

    merge sort python

---
+++ Sorting/Python/merge_sort.py
@@ -0,0 +1,12 @@
+def min_to_max(x, y):
+    if x < y:
+        return -1
+    elif x > y:
+        return 1
+    else:
+        return 0
+
+def merge_sort(arr):
+
+def merge_sort_helper(arr_1, arr_2):
+
--- Sorting/Ruby/merge_sort.rb
+++ Sorting/Ruby/merge_sort.rb
@@ -3,32 +3,28 @@ def merge_sort(arr, &prc)
     return arr if (arr.length == 1 || arr.empty?)
 
     middle_idx = arr.length / 2
-    left = merge_sort(arr.take(middle_idx))
-    right = merge_sort(arr.drop(middle_idx))
-    
-    merge_sort_helper(left, right, prc)
+    left = merge_sort(arr.take(middle_idx), &prc)
+    right = merge_sort(arr.drop(middle_idx), &prc)
+
+    merge_sort_helper(left, right, &prc)
 end 
 
 def merge_sort_helper(arr_1, arr_2, &prc) 
     sorted_arr = [] 
 
     until (arr_1.empty? || arr_2.empty?)
-        if arr_1.empty?
-            sorted_arr += arr_2 
-        elsif arr_2.empty? 
-            sorted_arr += arr_1 
-        else 
-            case (prc.call(arr_1[0], arr_2[0]))
-            when -1
-                sorted_arr.push(arr_1.shift)
-            when 0
-                sorted_arr.push(arr_1.shift)
-                sorted_arr.push(arr_2.shift)
-            when 1
-                sorted_arr.push(arr_2.shift)
-            end
+        case (prc.call(arr_1[0], arr_2[0]))
+        when -1
+            sorted_arr.push(arr_1.shift)
+        when 0
+            sorted_arr.push(arr_1.shift)
+            sorted_arr.push(arr_2.shift)
+        when 1
+            sorted_arr.push(arr_2.shift)
         end
     end
+
+    arr_1.empty? ? sorted_arr += arr_2 : sorted_arr += arr_1
 
     return sorted_arr
 end
