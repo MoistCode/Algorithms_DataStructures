d37c2d82dd6b8d78e3fb30e57fd63a6d715221b1
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-14 14:05:05 -0700

    python bfs

---
+++ Searching/Ruby/breadth_first_search.rb
@@ -0,0 +1,19 @@
+class Graph 
+    def initialize
+
+    end
+end
+
+class Node 
+    def initialize(value, graph, to_node, from_node)
+
+    end
+end
+
+graph = new Graph 
+
+node1 = new Node(1, graph)
+node2 = new Node(2, graph)
+node3 = new Node(3, graph)
+node4 = new Node(4, graph)
+node5 = new Node(5, graph)
