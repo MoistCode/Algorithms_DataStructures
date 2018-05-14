031e2eda6cd5f23dd0520e314fc86ed704f0ab0a
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-14 12:30:21 -0700

    need to work on piles

--- White_Board/Ruby/pile_of_boxes.rb
+++ White_Board/Ruby/pile_of_boxes.rb
@@ -2,22 +2,28 @@ def pile_of_boxes(total_boxes, max_boxes, split_num)
     return 0 if total_boxes == 0
     return 1 if total_boxes <= max_boxes
     num_per_pile = total_boxes / split_num
+    current_num_of_boxes = total_boxes
     arr = []
 
-    until arr.length == split_num || total_boxes == 0
-        if total_boxes - num_per_pile == 1
+    until current_num_of_boxes == 0
+        if current_num_of_boxes - num_per_pile == (num_per_pile + 1)
+            arr.push(num_per_pile)
             arr.push(num_per_pile + 1)
-            total_boxes -= (num_per_pile + 1)
-        elsif total_boxes - num_per_pile == -1
-            arr.push(num_per_pile - 1)            
-            total_boxes - (num_per_pile - 1)
+            current_num_of_boxes = 0
+        elsif current_num_of_boxes - num_per_pile == (num_per_pile - 1)
+            arr.push(num_per_pile)
+            arr.push(num_per_pile - 1)
+            current_num_of_boxes = 0
         else
-            arr.push(num_per_pile)            
-            total_boxes - num_per_pile
+            arr.push(num_per_pile)
+            current_num_of_boxes -= num_per_pile
         end
     end
 
-    arr.inject(0) { |acc, sub_boxes| acc += pile_of_boxes(sub_boxes, max_boxes, split_num) }
+    arr.reduce(0) { |acc, sub_boxes| acc += pile_of_boxes(sub_boxes, max_boxes, split_num) }
 end
 
-p pile_of_boxes(11, 2, 2)
+# p pile_of_boxes(11, 2, 2)
+# p pile_of_boxes(100, 1, 3)
+# p pile_of_boxes(64, 2, 4)
+# p pile_of_boxes(64, 2, 3)
