03437c187dc99268285ed5d980ae25cc0deae032
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-15 10:56:50 -0700

    min heap ruby

---
+++ Data_Structures/Ruby/min_heap.rb
@@ -0,0 +1,33 @@
+class MinHeap 
+
+    def initialize 
+
+    end
+
+    def insert(k)
+
+    end
+
+    def delete(k)
+
+    end
+
+    def get_min()
+
+    end
+
+    private
+
+    def heapify_up
+
+    end
+
+    def heapify_down 
+        
+    end
+
+    def get_parent 
+
+    end
+
+end
