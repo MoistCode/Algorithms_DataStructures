cd8edce5c1ca908bfc0c08822478b29ed54ef558
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-15 11:03:46 -0700

    min heap ruby

--- Data_Structures/Ruby/min_heap.rb
+++ Data_Structures/Ruby/min_heap.rb
@@ -1,11 +1,15 @@
 class MinHeap 
+    attr_accessor :heap, :size
 
     def initialize 
-
+        @heap = {}
+        @size = 0
     end
 
     def insert(k)
-
+        @heap[@size] = k
+        heapify_up(@size)
+        @size += 1
     end
 
     def delete(k)
@@ -26,8 +30,16 @@ class MinHeap
         
     end
 
-    def get_parent 
+    def get_parent(k)
 
+    end
+
+    def get_left_child(k)
+        left_child_key = (2 * k) + 1
+    end
+
+    def get_right_child(k)
+        right_child_key = (2 * k) + 2
     end
 
 end
