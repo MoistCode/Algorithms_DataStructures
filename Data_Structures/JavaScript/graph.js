934be66fe7b3391b3e56bfb606f736ba9ceb3ef8
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-29 14:07:10 -0700

    adjacency list

--- Data_Structures/JavaScript/graph.js
+++ Data_Structures/JavaScript/graph.js
@@ -1,3 +1,20 @@
-let vertexList = []
-let edgeList = []
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.headNode = new Node(null);
+    }
 
+    addNode() {
+
+    }
+}
+
+// Adjacency matrix
+
+// Adjacency list
