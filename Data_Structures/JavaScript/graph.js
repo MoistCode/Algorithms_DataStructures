a7813d641c8374d9489c2b0c3ad94b9a9beda945
Tommy Pham <phamtommylong1991@gmail.com>
2018-05-29 14:14:05 -0700

    custom linked list with graphs

--- Data_Structures/JavaScript/graph.js
+++ Data_Structures/JavaScript/graph.js
@@ -1,16 +1,37 @@
 class Node {
     constructor(data) {
         this.data = data;
-        this.prev = null;
         this.next = null;
     }
 }
 class CustomLinkedList {
-    constructor(vertex) {
-        this.headNode = new Node(null);
+    constructor(initialNode) {
+        this.headNode = initalNode;
     }
 
-    addNode() {
+    addNode(addedNode) {
+        let currentNode = this.headNode;
+        while(currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = addedNode;
+    }
 
+    printLinkedList() {
+        let currentNode = this.headNode;
+        while(currentNode.next != null) {
+            console.log(currentNode.data);
+            currentNode = currentNode.next;
+        }
     }
 }
+
+let listOfNodes = [
+    new CustomLinkedList(new Node(1)),
+    new CustomLinkedList(new Node(2)),
+    new CustomLinkedList(new Node(3)),
+    new CustomLinkedList(new Node(4)),
+    new CustomLinkedList(new Node(5)),
+    new CustomLinkedList(new Node(6)),
+];
+
