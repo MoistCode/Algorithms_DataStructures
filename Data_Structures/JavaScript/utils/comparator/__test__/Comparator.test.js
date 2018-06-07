8656ad93f8ea77e344c9c5cb8b043cf397052dd5
Tommy Pham <phamtommylong1991@gmail.com>
2018-06-07 13:17:39 -0700

    comparator test

---
+++ Data_Structures/JavaScript/utils/comparator/__test__/Comparator.test.js
@@ -0,0 +1,10 @@
+import Comparator from "../Comparator";
+
+describe("Comparator", () => {
+  it("should compare with default comparator function", () => {
+    const comparator = new Comparator();
+
+    expect(comparator.equal(0, 0)).toBeTruthy();
+    expect(compara.equal(0, 1)).toBeFalsey();
+  });
+});
