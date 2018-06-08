cb865cad93fa208d985699b16ed50b52da408ea5
Tommy Pham <phamtommylong1991@gmail.com>
2018-06-07 17:57:22 -0700

    permutations

---
+++ Whiteboards/rockpaperscissor.js
@@ -0,0 +1,15 @@
+/*
+  Find all possible combinations in a rock-paper-scissor game.
+  Given rounds, find all combinations.
+  Ex. 
+    Input = 2
+    Output = [
+      'rr', 'rp', 'rs', 'pr', 'pp', 'ps', 'sr', 'sp', 'ss
+    ]
+*/
+
+const rockPaperScissor = rounds => {
+  let results = [];
+
+  const getRPSCombinations = (alreadyDone, rounds) => {};
+};
