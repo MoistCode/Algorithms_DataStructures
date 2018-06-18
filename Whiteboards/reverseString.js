42d4ffcdd6dcd556dde590f38b54ef74e4bd841b
Tommy Pham <phamtommylong1991@gmail.com>
2018-06-18 16:37:26 -0700

    refactor

--- Whiteboards/reverseString.js
+++ Whiteboards/reverseString.js
@@ -1,15 +1,13 @@
 function reverseString1(str) {
   let arr = [...str];
-  let newArr = arr.map((_, i, arr) => {
-    return arr[arr.length - 1 - i];
-  });
+  let newArr = arr.map((_, i, arr) => arr[arr.length - 1 - i]);
   return newArr.join("");
 }
 
 function reverseString2(str) {
-  return str.split("").reduce((reversedString, character) => {
-    return character + reversedString;
-  }, "");
+  return str
+    .split("")
+    .reduce((reversedString, character) => character + reversedString, "");
 }
 
 console.log(reverseString2("cowman"));
