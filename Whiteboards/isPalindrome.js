0f6c5fb4d376c3fe01f023026011875a478b2c15
Tommy Pham <phamtommylong1991@gmail.com>
2018-06-18 16:50:42 -0700

    palindrome 2

--- Whiteboards/isPalindrome.js
+++ Whiteboards/isPalindrome.js
@@ -5,3 +5,16 @@ function isPalindrome1(str) {
     .join("");
   return revString === str;
 }
+
+function isPalindrome2(str) {
+  let isPali = true;
+  let caseStr = str.toLowerCase();
+  let arr = caseStr.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != arr[arr.length - 1 - i]) {
+      isPali = false;
+      break;
+    }
+  }
+  return isPali;
+}
