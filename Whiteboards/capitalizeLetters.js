fa46ac3611ac7238b21a48a919c4872f299d8bc6
Tommy Pham <phamtommylong1991@gmail.com>
2018-06-18 17:03:53 -0700

    cap 1

---
+++ Whiteboards/capitalizeLetters.js
@@ -0,0 +1,10 @@
+function capitalizeLetters1(str) {
+  const strArr = str.toLowerCase().split(" ");
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] =
+      strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  }
+  return strArr.join(" ");
+}
+
+console.log(capitalizeLetters1("cowman is nigh"));
